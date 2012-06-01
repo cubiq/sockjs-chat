7a0440367f87eaea513348957da7e32be1cd453d
Matteo Spinelli <matteo@cubiq.org>
2012-06-01 13:16:44 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 sockjs-chat
 ===========
 
-Lame sockJS chat demo
+Lame [SockJS](http://sockjs.org) chat demo.
+
+Install SockJS, run the socket server:
+
+	node socket.js
+
+Point your browser to index.html.
+
+Enjoy.
+
+This is just a personal exercise with SockJS (due to the lack of demos), of course, not ready for production.
---
+++ socket.js
@@ -0,0 +1,55 @@
+var http = require('http'),
+	sockjs = require('sockjs'),
+	server = http.createServer(),
+	buffer = [],
+	clients = {};
+
+function whisper (id, message) {
+	if ( !clients[id] ) return;
+
+	clients[id].write( JSON.stringify(message) );
+}
+
+function broadcast (message, exclude) {
+	for ( var i in clients ) {
+		if ( i != exclude ) clients[i].write( JSON.stringify(message) );
+	}
+}
+
+function onConnection (conn) {
+	clients[conn.id] = conn;
+
+	broadcast({ type: 'newUser' }, conn.id);
+	whisper(conn.id, { type: 'history', message: buffer, id: conn.id });
+
+	conn.on('data', function onDataCB (data) {
+		data = JSON.parse(data);
+
+		if ( data.type == 'text' ) {
+			if ( !data.message ) return;
+
+			data.message = data.message.substr(0, 128);
+
+			if ( buffer.length > 15 ) buffer.shift();
+			buffer.push(data.message);
+
+			broadcast({ type: 'message', message: data.message, id: conn.id });
+		}
+
+		if ( data.type == 'username' ) {
+			
+		}
+	});
+
+	conn.on('close', function onCloseCB () {
+		delete clients[conn.id];
+
+		broadcast({ type: 'userLeft' });
+	});
+}
+
+webSockets = sockjs.createServer();
+webSockets.on('connection', onConnection);
+
+webSockets.installHandlers(server, { prefix:'/echo' } );
+server.listen(9999, '0.0.0.0');
